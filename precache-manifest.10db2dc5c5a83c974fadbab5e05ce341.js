b1603c85f7175ef52d30e548ff54fc1f4bd3e1c1
serhii.horobets <serhii.horobets@itera.no>
2019-04-26 07:50:34 +0300

    Updates

---
+++ precache-manifest.10db2dc5c5a83c974fadbab5e05ce341.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8a117f01a5f51c2e71359345ac1300",
+    "url": "/shop-agg/index.html"
+  },
+  {
+    "revision": "dc7b9b2a182a505caa90",
+    "url": "/shop-agg/static/js/2.3297f76e.chunk.js"
+  },
+  {
+    "revision": "8a3b2377138a6e9ce961",
+    "url": "/shop-agg/static/js/main.845be21e.chunk.js"
+  },
+  {
+    "revision": "0c048c697d7de56354d1",
+    "url": "/shop-agg/static/js/runtime~main.3d910e0f.js"
+  }
+]);
